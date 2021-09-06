3cede5f6c3b11ed147590e3baeab1ca6e11c3388
jiho-bae <jiho@dgu.ac.kr>
2021-09-07 00:23:49 +0900

    Fix: 프로젝트 구조 변경

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 const PORT = 3000;
-
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public', 'index.html')));
 
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
