9bb9a59a65055431991fb9b14818283f8cfeaf66
jiho-bae <jiho@dgu.ac.kr>
2021-09-07 00:23:31 +0900

    Add: 웹팩 html파일 생성

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,20 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 const MODE = 'development';
 
 module.exports = {
-  entry: './src/front/js/main.js',
+  entry: './src/client/js/main.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public'),
     filename: 'js/main.js',
     clean: true,
   },
   mode: MODE,
-  plugins: [new MiniCssExtractPlugin({ filename: 'css/style.css' })],
+  plugins: [
+    new MiniCssExtractPlugin({ filename: 'css/style.css' }),
+    new HtmlWebpackPlugin({ template: 'src/app.html' }),
+  ],
   watch: true,
   devtool: MODE === 'development' ? 'source-map' : '',
   module: {
